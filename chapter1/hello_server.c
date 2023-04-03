c85e706687dfc6c36e6cb082dc18f4c0977f4a2c
水无月 <65015898+Vivmoy@users.noreply.github.com>
2023-04-03 19:52:20 +0800

    learning

---
+++ chapter1/hello_server.c
@@ -0,0 +1,78 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<unistd.h>
+#include<arpa/inet.h>
+#include<sys/socket.h>
+
+void error_handling(char* message);
+
+/*
+struct sockaddr_in
+{
+    sa_family_t        sin_family;      //地址族
+    uint16_t           sin_port;        //16位TCP/UDP端口号
+    struct in_addr     sin_addr;        //32位IP地址
+    char               sin_zero[8];     //不使用
+}
+
+struct in_addr
+{
+    in_addr_t          s_addr;          //32位IPv4地址
+}
+
+struct sockaddr
+{
+    sa_family_t        sin_family;      //地址族
+    char               sa_data[14];     //地址信息
+}
+*/
+int main(int argc, char* argv[])
+{
+    int serv_sock;
+    int clnt_sock;
+
+    struct sockaddr_in serv_addr;
+    struct sockaddr_in clnt_addr;
+    socklen_t clnt_addr_size;
+
+    char message[] = "Hello world!";  
+   
+    if(argc != 2)
+    {
+        printf("Usage: %s <port>\n",argv[0]);   
+        exit(1);
+    }
+
+    serv_sock = socket(PF_INET,SOCK_STREAM,0);
+    if(serv_sock == -1)
+        error_handling("socket() error");
+
+    memset(&serv_addr,0,sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serv_addr.sin_port = htons(atoi(argv[1]));
+
+    if(bind(serv_sock,(struct sockaddr*)&serv_addr,sizeof(serv_addr)) == -1)
+        error_handling("bind() error");
+
+    if(listen(serv_sock,5) == -1 )
+        error_handling("listen() error");
+
+    clnt_addr_size = sizeof(clnt_addr);
+    clnt_sock = accept(serv_sock,(struct sockaddr*)&clnt_addr,&clnt_addr_size);        
+    if(clnt_sock == -1)
+        error_handling("accept() error");
+
+    write(clnt_sock,message,sizeof(message));
+    close(clnt_sock);
+    close(serv_sock);
+    return 0;
+}
+
+void error_handling(char* message)
+{
+    fputs(message,stderr);
+    fputc('\n',stderr);
+    exit(1);
+}
